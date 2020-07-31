34d5042a5b0e93b63418f1a9b3ab585d5190e560
vimal-gayakwad <gayakwadvimal1@gmail.com>
2020-07-31 20:47:55 +0530

    updated

---
+++ app/src/main/java/com/example/ssbs/patient/PatientRegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.ssbs.patient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ssbs.R;
+
+public class PatientRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_registration);
+    }
+}
